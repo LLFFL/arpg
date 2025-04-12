1021dd37c25859cfe26b6fcdcb673be068687be2
msernoskie <mark.sernoskie@gmail.com>
2025-04-11 21:02:15 -0400

    My branch with projectiles

---
+++ scenes/components/Projectile_HurtBox.gd
@@ -0,0 +1,21 @@
+class_name Projectile_HurtBox
+extends Area2D
+
+signal projectile_hit_enemy
+
+@onready var projectile : Projectile = get_owner()
+
+func _ready() -> void:
+	self.add_to_group("projectiles")
+	area_entered.connect(on_area_entered)
+	
+	
+func on_area_entered(area: Area2D):
+	if area is Hitbox:
+		var attack := Attack.new()
+		
+		attack.damage = projectile.damage
+		
+		area.damage(attack)
+		
+		projectile_hit_enemy.emit()
