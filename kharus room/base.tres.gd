4cf07c163b7617c0d36659d2129e2e6ca4a5fe4d
Kharus <kharusyan@gmail.com>
2025-04-13 14:48:58 -0700

    a

--- kharus room/base.tres.gd
+++ kharus room/base.tres.gd
@@ -6,11 +6,19 @@ var minion_rate: int = 2
 var player_side: bool = false
 @export var SpawnOver: bool = false
 const BAT = preload("res://scenes/Bat.tscn")
-
+@export var MainBase: bool
+@export var LeftBase:bool
+@export var RightBase:bool
 #TODO how to make base damage with stats and hurtboxes etc
 
 func _ready():
 	$SpawnTimer.start(minion_rate)
+	if (MainBase):
+		$SpawnLocation.position = Vector2(0, 100)
+	if (LeftBase):
+		$SpawnLocation.position = Vector2(0, 100)
+	if (RightBase):
+		$SpawnLocation.position = Vector2(0, -100)
 
 func initialize(coords, side):
 	enemy_base_position = coords
