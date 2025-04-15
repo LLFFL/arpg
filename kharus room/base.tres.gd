d15ad86dd7eedd4a4a465521939f8bdcf9e358d9
Kharus <kharusyan@gmail.com>
2025-04-14 21:45:07 -0700

    a

--- kharus room/base.tres.gd
+++ kharus room/base.tres.gd
@@ -3,7 +3,7 @@ extends Node2D
 var base_positions: Dictionary
 var resource_rate: int = 10
 var minion_rate: int = 10
-var minions_per_wave: int = 3
+var minions_per_wave: int = 4
 const BAT = preload("res://scenes/Bat.tscn")
 @onready var stats = $StatsComponent
 @onready var hurtbox = $HurtBox
@@ -35,16 +35,19 @@ func initialize(base_positionss: Dictionary):
 	base_positions = base_positionss # this is not a a typo
 
 func spawn_minion(player_side:bool):
-	var bat_spawn = BAT.instantiate()
-	bat_spawn.initialize(player_side, base_positions)
-	get_parent().add_child(bat_spawn)
-	bat_spawn.position = bat_spawn_location
+	var i = 0
+	while(i < minions_per_wave):
+		i += 1
+		var bat_spawn = BAT.instantiate()
+		bat_spawn.initialize(player_side, base_positions)
+		get_parent().add_child(bat_spawn)
+		bat_spawn.position = bat_spawn_location
 
 func take_damage(attack: Attack) -> void:
 	print(attack.damage)
 	stats.health -= attack.damage
 	print(stats.health)
-	hurtbox.start_invincibility(0.4)
+	hurtbox.start_invincibility(0.2)
 	hurtbox.create_hit_effect()
 
 func _on_spawn_timer_timeout() -> void:
