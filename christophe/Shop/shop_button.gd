0a0648ebee1a4f442e278e23c18efe2b6c0a1900
Christophe Marois <>
2025-04-19 10:13:33 -0400

    added maximum amount of buying to shop items

--- christophe/Shop/shop_button.gd
+++ christophe/Shop/shop_button.gd
@@ -1,15 +1,33 @@
 extends Control
 
+enum UpgradeType { DAMAGE, MOVEMENT_SPEED, DEFENCE, LUCK, UNITS, SLASH}
+@export var upgrade_type: UpgradeType = UpgradeType.DAMAGE
+@export var upgrade_id: String = "damage_boost"
+
+@export_category("Costs and Stats")
 @export var upgrade_cost: int = 25
 @export var upgrade_value: int = 2
-@export var upgrade_id: String = "damage_boost"
+
+## Current level of the item (how many time it was bought)
+@export var current_level: int = 0
+
+## Maximum level of the item (how many time it can be bought)
+@export var max_level:int = 3
+
+@export_category("Visuals")
 @export var icon_path: Texture2D
+
+
+@export_category("Sounds")
 @export var buying_sound:AudioStream
 @export var cantbuy_sound:AudioStream
 
-enum UpgradeType { DAMAGE, MOVEMENT_SPEED, DEFENCE, LUCK }
 
-@export var upgrade_type: UpgradeType = UpgradeType.DAMAGE
+
+# limit number of buying
+# unit upgrade n amount bought
+# slash attack upgrade (x2)
+
 var tween:Tween
 
 
@@ -17,14 +35,20 @@ func _ready():
 	%BuyButton.pressed.connect(_on_pressed)
 	%CostLabel.text = str(upgrade_cost)
 	%ItemIcon.texture = icon_path
+	%LimitLabel.text = str(current_level) + "/" + str(max_level)
 
 func _on_pressed() -> void:
 	# Not enough gold
+	if current_level==max_level:
+		play_max_level_reached_animation()
+		return
 	if PlayerManager.player.stats.gold < upgrade_cost:
 		play_not_enough_gold_animation()
 		return
-		
+	
 	# buy the item
+	current_level+=1
+	%LimitLabel.text = str(current_level) + "/" + str(max_level)
 	PlayerManager.player.stats.gold -= upgrade_cost
 	play_buying_animation()
 	match upgrade_type:
@@ -70,3 +94,20 @@ func play_not_enough_gold_animation():
 	tween.tween_property(%BuyButton,"position:x",-2,0.05)
 	tween.tween_property(%BuyButton,"position:x",0,0.025)
 	tween.tween_property(%CostLabel,"modulate",Color("white"),0.025)
+
+func play_max_level_reached_animation():
+	if tween:
+		tween.kill()
+		#%BuyButton.position.x = 0
+		self.scale = Vector2(1,1)
+	$AudioStreamPlayer2D.stream = cantbuy_sound
+	$AudioStreamPlayer2D.play()
+	tween = create_tween().set_ease(Tween.EASE_OUT)
+	tween.tween_property(%BuyButton,"position:x",0,0.005)
+	tween.tween_property(%LimitLabel,"modulate",Color("white"),0.005)
+	tween.tween_property(%LimitLabel,"modulate",Color("ff5454"),0.025)
+	tween.tween_property(%BuyButton,"position:x",-2,0.05)
+	tween.tween_property(%BuyButton,"position:x",2,0.05)
+	tween.tween_property(%BuyButton,"position:x",-2,0.05)
+	tween.tween_property(%BuyButton,"position:x",0,0.025)
+	tween.tween_property(%LimitLabel,"modulate",Color("white"),0.025)
